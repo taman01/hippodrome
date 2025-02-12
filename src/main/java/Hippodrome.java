8b0a7599affdb339b9f4c264072f4a406daa62ee
taman01 <tomaevgenij@yandex.ru>
2025-02-12 14:34:27 +0300

    " 2 мини-проект по 3 модулю"

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -8,14 +11,19 @@ public class Hippodrome {
 
     private final List<Horse> horses;
 
+    private static final Logger logger = LoggerFactory.getLogger(Hippodrome.class);
+
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            logger.error("Horse list is null!");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            logger.error("Horse list is empty!");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        logger.debug("Создание Hippodrome, лошадей [{}]", this.horses.size());
     }
 
     public List<Horse> getHorses() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -14,8 +19,9 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        logger.info("Начало скачек. Количество участников: {}", hippodrome.getHorses().size());
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 4; i++) {
             hippodrome.move();
             watch(hippodrome);
             TimeUnit.MILLISECONDS.sleep(200);
@@ -23,6 +29,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        logger.info("Окончание скачек. Победитель: {}", hippodrome.getWinner().getName());
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
